fd81ebe22aef0aa37a0771f0a20f8927027dab7c
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2022-10-06 10:01:31 -0300

    finalizando a sidebar, deixando estilizada e responsiva

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import Avatar from '../img/eu.png'
 import '../styles/components/sidebar.sass'
 import SocialNetworks from './SocialNetworks'
+import InformationContainer from './InformationContainer'
 
 const Sidebar = () => {
   return <aside id="sidebar">
     <img src={Avatar} alt="imagem perfil" />
     <p className="title">Desenvolvedor FullStack</p>
     <SocialNetworks />
-    <p>informações de contato</p>
+    <InformationContainer />
     <a href="" className="btn">Baixar Curriculo</a>
   </aside>
 }
